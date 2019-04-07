75aefde09a62ef428b9bb156d2f690ec41db981a
unknown <itsasiam.business@gmail.com>
2019-04-06 22:44:42 -0400

    final changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+
+var array = ["Hakuna", "Simba", "Lion", "Safari"]
+for (var i = 0; i < array.length; i++) {
+
+}
+
+
+
+var
+
+
+    $("#Click").on("click", function () {
+
+    });
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</style >
